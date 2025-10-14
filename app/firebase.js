fefbee35895457765b721e3f7a53b25cc29633cb
haigarcia08 <haidiegarcia.24@gmail.com>
2025-10-14 11:57:12 +0800

    haidie-auth

--- app/firebase.js
+++ app/firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
-
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyA6Wr9IKnvVTki4LmxdCTnFjw13JlIwBDU",
+  authDomain: "haidie-auth.firebaseapp.com",
+  projectId: "haidie-auth",
+  storageBucket: "haidie-auth.firebasestorage.app",
+  messagingSenderId: "632369556369",
+  appId: "1:632369556369:web:8724bf6784ddd67b100830",
+  measurementId: "G-R4MMQMEWHF"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export auth = getAuth(app);
